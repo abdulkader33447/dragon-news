19fef9c6d42f93fba057b9ae95c31709665c3830
abdulkader33447 <abdulkader33447@gmail.com>
2025-04-30 21:59:57 +0600

    added date formate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import logo from "../assets/logo.png";
+import { format } from "date-fns";
 
 const Header = () => {
   return (
     <div className="flex justify-center flex-col items-center gap-3">
       <img src={logo} alt="logo" />
-      <p>Journalism Without Fear or Favour</p>
+      <p className="text-accent">Journalism Without Fear or Favour</p>
+
+      <p className="text-accent text-lg">{format(new Date(), "EEEE, MMMM-dd, yyyy")}</p>
     </div>
   );
 };
